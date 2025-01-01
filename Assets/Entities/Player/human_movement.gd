463d6834f888acde5fd59178b4faec942603a3bb
mojunikatowynick <148270188+mojunikatowynick@users.noreply.github.com>
2025-01-01 13:16:34 +0100

    change in state machine
    changed that basic movement is kept in main player scene adn added states that will alter it and add appropriate animation

--- Assets/Entities/Player/human_movement.gd
+++ Assets/Entities/Player/human_movement.gd
@@ -10,25 +10,8 @@ func Enter():
 	
 func Physics_update(_delta: float):
 	
-	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and player.is_on_floor():
-		player.velocity.y = player.JUMP_VELOCITY
+	if player.is_on_floor() == false:
 		Transitioned.emit(self, "HumanJump")
-
-	# Get the input direction and handle the movement/deceleration.
-	var input_dir = Input.get_vector("StrafeLeft", "StrafeRight", "Forward", "Back")
-	var direction = (player.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		if Input.is_action_pressed("Sprint"):
-			player.velocity.x = direction.x * player.SPEED * player.SRINT_MULTI
-			player.velocity.z = direction.z * player.SPEED * player.SRINT_MULTI
-		else:
-			player.velocity.x = direction.x * player.SPEED
-			player.velocity.z = direction.z * player.SPEED
-	else:
-		#breaking speed from current vel to standstill "0" in time speed: change speed to fraction (0.1) for slowly slowing down
-		player.velocity.x = move_toward(player.velocity.x, 0, player.SPEED)
-		player.velocity.z = move_toward(player.velocity.z, 0, player.SPEED)
 	
 	animation()
 	
--- Assets/Entities/Player/player.gd
+++ Assets/Entities/Player/player.gd
@@ -30,6 +30,30 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
+	
+		# Handle jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("StrafeLeft", "StrafeRight", "Forward", "Back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		if Input.is_action_pressed("Sprint"):
+			velocity.x = direction.x * SPEED * SRINT_MULTI
+			velocity.z = direction.z * SPEED * SRINT_MULTI
+		else:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+	else:
+		#breaking speed from current vel to standstill "0" in time speed: change speed to fraction (0.1) for slowly slowing down
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	
+	
+	
 	_update_camera(delta)
 	
 	move_and_slide()
@@ -62,6 +86,8 @@ func _update_camera(delta):
 	
 	_rotation_input = 0.0
 	_tilt_input = 0.0
+
+
 
 func toggle_crouch():
 	if _is_crouching == true: 
